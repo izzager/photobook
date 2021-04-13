3906f2c208c77752b93f46e968b1c82d431fce0f
Tatyana Elesina <telesina@griddynamics.com>
2021-04-13 15:43:57 +0400

    Implement album service

--- src/main/java/com/example/photobook/service/AlbumService.java
+++ src/main/java/com/example/photobook/service/AlbumService.java
@@ -2,11 +2,12 @@ package com.example.photobook.service;
 
 import com.example.photobook.dto.AlbumDto;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 public interface AlbumService {
-    List<AlbumDto> findAllAlbum();
+    List<AlbumDto> findAllAlbums();
     AlbumDto createAlbum(AlbumDto albumDto);
     AlbumDto deleteAlbum(Long albumId);
-    void downloadAsZip(Long albumId);
+    void downloadAsZip(Long albumId, HttpServletResponse response);
 }
--- src/main/java/com/example/photobook/service/impl/AlbumServiceImpl.java
+++ src/main/java/com/example/photobook/service/impl/AlbumServiceImpl.java
@@ -1,30 +1,70 @@
 package com.example.photobook.service.impl;
 
 import com.example.photobook.dto.AlbumDto;
+import com.example.photobook.entity.Album;
+import com.example.photobook.helper.AlbumRepositoryHelper;
+import com.example.photobook.mapperToEntity.AlbumMapper;
+import com.example.photobook.repository.AlbumRepository;
 import com.example.photobook.service.AlbumService;
+import com.example.photobook.util.FileZipper;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class AlbumServiceImpl implements AlbumService {
+
+    private final AlbumRepository albumRepository;
+    private final ModelMapper modelMapper;
+    private final AlbumRepositoryHelper albumRepositoryHelper;
+    private final AlbumMapper albumMapper;
+
+    @Value("${photobookapp.downloading-directory}")
+    private String pathToFiles;
+
     @Override
-    public List<AlbumDto> findAllAlbum() {
-        return null;
+    public List<AlbumDto> findAllAlbums() {
+        return albumRepository.findAll()
+                .stream()
+                .map(album -> modelMapper.map(album, AlbumDto.class))
+                .collect(Collectors.toList());
     }
 
     @Override
     public AlbumDto createAlbum(AlbumDto albumDto) {
-        return null;
+        Album album = albumMapper.toEntity(albumDto);
+        return modelMapper.map(albumRepository.save(album), AlbumDto.class);
     }
 
     @Override
     public AlbumDto deleteAlbum(Long albumId) {
-        return null;
+        Album album = albumRepositoryHelper.ensureAlbumExists(albumId);
+        albumRepository.delete(album);
+        return modelMapper.map(album, AlbumDto.class);
     }
 
     @Override
-    public void downloadAsZip(Long albumId) {
+    public void downloadAsZip(Long albumId, HttpServletResponse response) {
+        Album album = albumRepositoryHelper.ensureAlbumExists(albumId);
+        List<String> files = album.getPhotos()
+                .stream()
+                .map(photo -> pathToFiles + photo.getPhotoName())
+                .collect(Collectors.toList());
 
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.addHeader("Content-Disposition",
+                "attachment; filename=\"" + album.getAlbumName() + ".zip\"");
+        try {
+            FileZipper.zip(files, response.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
