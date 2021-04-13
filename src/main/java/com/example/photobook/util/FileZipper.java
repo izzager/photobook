4cbeaa3296c1b465cb965a17b001afdea4e92410
Tatyana Elesina <telesina@griddynamics.com>
2021-04-13 15:42:40 +0400

    Refactor zipper

--- src/main/java/com/example/photobook/util/FileZipper.java
+++ src/main/java/com/example/photobook/util/FileZipper.java
@@ -1,9 +1,12 @@
 package com.example.photobook.util;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletOutputStream;
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -11,19 +14,14 @@ import java.util.zip.ZipOutputStream;
 @Component
 public class FileZipper {
     public static void zip(List<String> files, ServletOutputStream sos) {
-        try(ZipOutputStream zipOut = new ZipOutputStream(sos)) {
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(sos)) {
             for (String file : files) {
                 File fileToZip = new File(file);
                 FileInputStream fis = new FileInputStream(fileToZip);
-                ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
-                zipOut.putNextEntry(zipEntry);
-
-                byte[] bytes = new byte[1024];
-                int length;
-                while((length = fis.read(bytes)) >= 0) {
-                    zipOut.write(bytes, 0, length);
-                }
+                zipOutputStream.putNextEntry(new ZipEntry(fileToZip.getName()));
+                IOUtils.copy(fis, zipOutputStream);
                 fis.close();
+                zipOutputStream.closeEntry();
             }
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
