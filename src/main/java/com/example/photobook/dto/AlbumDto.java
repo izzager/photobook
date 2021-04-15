150ab47c81adf816a7f8653e4316e903185a806a
Tatyana Elesina <telesina@griddynamics.com>
2021-04-15 20:49:58 +0400

    Add validations to album dto

--- src/main/java/com/example/photobook/dto/AlbumDto.java
+++ src/main/java/com/example/photobook/dto/AlbumDto.java
@@ -3,10 +3,17 @@ package com.example.photobook.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+
 @Data
 @NoArgsConstructor
 public class AlbumDto {
+
+    @Null(message = "Album id must be null")
     private Long id;
 
+    @NotBlank(message = "Album name must be not blank")
     private String albumName;
+
 }
