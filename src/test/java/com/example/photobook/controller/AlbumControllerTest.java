f312ac14f83502882005c110a4e2b9f10763afd0
Tatyana Elesina <telesina@griddynamics.com>
2021-04-19 18:17:16 +0400

    Add tests for album controller

---
+++ src/test/java/com/example/photobook/controller/AlbumControllerTest.java
@@ -0,0 +1,92 @@
+package com.example.photobook.controller;
+
+import com.example.photobook.dto.CreateAlbumDto;
+import com.example.photobook.exception.ControllerExceptionHandler;
+import com.example.photobook.service.AlbumService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static com.example.photobook.TestConstants.ALBUM_ID;
+import static com.example.photobook.TestConstants.ALBUM_NAME;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class AlbumControllerTest {
+
+    private MockMvc mvc;
+
+    @InjectMocks
+    private AlbumController albumController;
+
+    @Mock
+    private AlbumService albumService;
+
+    @BeforeEach
+    public void setup() {
+        JacksonTester.initFields(this, new ObjectMapper());
+        mvc = MockMvcBuilders.standaloneSetup(albumController)
+                .setControllerAdvice(new ControllerExceptionHandler())
+                .build();
+    }
+
+    @Test
+    public void findAllAlbums_passes() throws Exception {
+        MockHttpServletResponse response = mvc
+                .perform(MockMvcRequestBuilders.get("/api/albums"))
+                .andReturn().getResponse();
+
+        assertEquals(response.getStatus(), HttpStatus.OK.value());
+    }
+
+    @Test
+    public void deleteAlbum_passes() throws Exception {
+        MockHttpServletResponse response = mvc
+                .perform(MockMvcRequestBuilders.delete("/api/albums/{id}", ALBUM_ID))
+                .andReturn().getResponse();
+        assertEquals(response.getStatus(), HttpStatus.NO_CONTENT.value());
+    }
+
+    @Test
+    public void createAlbum_passes() throws Exception {
+        CreateAlbumDto albumDto = new CreateAlbumDto();
+        albumDto.setAlbumName(ALBUM_NAME);
+
+        MockHttpServletResponse response = mvc
+                .perform(MockMvcRequestBuilders
+                        .post("/api/albums")
+                        .content(new ObjectMapper().writeValueAsString(albumDto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        assertEquals(response.getStatus(), HttpStatus.OK.value());
+    }
+
+    @Test
+    public void createAlbum_blankAlbumName_throwsHttpCode400() throws Exception {
+        CreateAlbumDto albumDto = new CreateAlbumDto();
+
+        MockHttpServletResponse response = mvc
+                .perform(MockMvcRequestBuilders
+                        .post("/api/albums")
+                        .content(new ObjectMapper().writeValueAsString(albumDto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        assertEquals(response.getStatus(), HttpStatus.BAD_REQUEST.value());
+    }
+
+}
