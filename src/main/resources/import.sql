d52e589c595e8eb1ae24f35af7676972134d6a15
Tatyana Elesina <elesina.tan@yandex.ru>
2021-04-27 19:09:45 +0400

    Add records with links

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,7 @@
 INSERT INTO album (album_name) VALUES ('random');
-INSERT INTO album (album_name) VALUES ('something');
+INSERT INTO album (album_name) VALUES ('cats');
 
 INSERT INTO photo (load_date, load_source, photo_name, album_id) VALUES ('2021-04-13 15:56:02', 'device', 'pom.xml', 1);
 INSERT INTO photo (load_date, load_source, photo_name, album_id) VALUES ('2021-04-13 15:56:02', 'device', 'README.md', 1);
+INSERT INTO photo (load_date, load_source, photo_name, album_id) VALUES ('2021-04-27 16:33:44', 'https://i.pinimg.com/474x/a0/1e/b9/a01eb920157d569f0c214bc48ef1dec4.jpg', 'sleepy cat.jpg', 2);
+INSERT INTO photo (load_date, load_source, photo_name, album_id) VALUES ('2021-04-27 17:53:34', 'https://snob.ru/i/indoc/user_32134/6ba5ee528e106b4edbae938462f6b65a.jpg', 'high quality cat.jpg', 2);
