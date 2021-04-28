334872a11bf42b5aae6075636491e2466d9437cb
Tatyana Elesina <elesina.tan@yandex.ru>
2021-04-28 13:33:53 +0400

    Fix

--- src/main/java/com/example/photobook/job/LocalFileDownloadingJob.java
+++ src/main/java/com/example/photobook/job/LocalFileDownloadingJob.java
@@ -4,6 +4,7 @@ import com.example.photobook.entity.Photo;
 import com.example.photobook.util.DownloadingStatusHelper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.MediaType;
@@ -23,6 +24,7 @@ import static java.nio.file.StandardOpenOption.WRITE;
 
 @Configuration
 @EnableScheduling
+@Profile("!default")
 public class LocalFileDownloadingJob {
 
     private final DownloadingStatusHelper downloadingStatusHelper;
