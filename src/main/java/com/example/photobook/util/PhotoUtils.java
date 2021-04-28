b5e556ad8ac4a3346ab0e95a0be5faaa550e41ee
Tatyana Elesina <elesina.tan@yandex.ru>
2021-04-28 11:46:09 +0400

    Replace custom method by RandomStringUtils method

--- src/main/java/com/example/photobook/util/PhotoUtils.java
+++ src/main/java/com/example/photobook/util/PhotoUtils.java
@@ -3,38 +3,25 @@ package com.example.photobook.util;
 import com.example.photobook.dto.UploadPhotoDto;
 import com.example.photobook.entity.Photo;
 import org.apache.commons.io.FilenameUtils;
-import org.springframework.stereotype.Component;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Random;
-
-@Component
 public class PhotoUtils {
 
-    private static String generatingRandomString() {
-        int leftLimit = 48; // numeral '0'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 30;
-        Random random = new Random();
-        return random.ints(leftLimit, rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                .limit(targetStringLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-    }
+    private static final int suffixLength = 30;
 
     public static String buildPhotoName(UploadPhotoDto uploadPhotoDto, MultipartFile file) {
-        return uploadPhotoDto.getPhotoName() + generatingRandomString() +
+        return uploadPhotoDto.getPhotoName() + RandomStringUtils.randomAlphanumeric(suffixLength) +
                 "." + FilenameUtils.getExtension(file.getOriginalFilename());
     }
 
     public static String buildPhotoName(UploadPhotoDto uploadPhotoDto) {
-        return uploadPhotoDto.getPhotoName() + generatingRandomString() +
+        return uploadPhotoDto.getPhotoName() + RandomStringUtils.randomAlphanumeric(suffixLength) +
                 "." + FilenameUtils.getExtension(uploadPhotoDto.getLink());
     }
 
     public static String buildPhotoName(Photo photo) {
-        return photo.getPhotoName() + generatingRandomString() +
+        return photo.getPhotoName() + RandomStringUtils.randomAlphanumeric(suffixLength) +
                 "." + FilenameUtils.getExtension(photo.getLoadSource());
     }
 
