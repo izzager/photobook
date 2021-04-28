24f456d5f7cc1c299ab6f30c6ece1a333c10909c
Tatyana Elesina <elesina.tan@yandex.ru>
2021-04-28 12:55:17 +0400

    Add WebClient bean

--- src/main/java/com/example/photobook/configuration/PhotobookConfig.java
+++ src/main/java/com/example/photobook/configuration/PhotobookConfig.java
@@ -5,6 +5,7 @@ import com.example.photobook.converterToDto.PhotoDtoConverter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class PhotobookConfig {
@@ -16,5 +17,10 @@ public class PhotobookConfig {
         modelMapper.addConverter(albumDtoConverter);
         modelMapper.addConverter(photoDtoConverter);
         return modelMapper;
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
     }
 }
