ebd5801ce277862e2abc9f47aa5df39b545086d0
telesina <elesina.tan@yandex.ru>
2021-04-29 18:11:54 +0400

    Add PhotoRepositoryHelper

---
+++ src/main/java/com/example/photobook/helper/PhotoRepositoryHelper.java
@@ -0,0 +1,28 @@
+package com.example.photobook.helper;
+
+import com.example.photobook.entity.Photo;
+import com.example.photobook.exception.ResourceNotFoundException;
+import com.example.photobook.repository.PhotoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PhotoRepositoryHelper {
+
+    private final PhotoRepository photoRepository;
+
+    public Photo ensurePhotoExists(Long photoId) {
+        return photoRepository
+                .findById(photoId)
+                .orElseThrow(() -> new ResourceNotFoundException("Photo not found"));
+    }
+
+    public Photo ensurePhotoExists(Long photoId, Long albumId) {
+        return photoRepository
+                .findById(photoId)
+                .filter(photo -> photo.getAlbum().getId().equals(albumId))
+                .orElseThrow(() -> new ResourceNotFoundException("Photo not found"));
+    }
+
+}
